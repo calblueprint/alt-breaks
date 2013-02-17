2206a4563b9680fb206673afe8693db3a791e715
callmemc <callmemc@gmail.com>
2013-02-16 17:31:35 -0800

    new rails app waddup

---
+++ altbreaks/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Altbreaks::Application.config.secret_token = '8781bb179f3599db38a97275c6bb20f407363d77bf0f4cc0e43b090c098a2a73bcec2a7fdb7c9dfa60dba9978a32dc850b0ec486693424d765efc57c8e6978fb'
---
+++ altbreaks/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Altbreaks::Application.config.session_store :cookie_store, key: '_altbreaks_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Altbreaks::Application.config.session_store :active_record_store
