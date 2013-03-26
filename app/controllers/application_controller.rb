5e35a266da744c6bd6bfcaefaadbd66c878469df
callmemc <callmemc@gmail.com>
2013-03-26 09:49:48 -0700

    redirect to dashboard after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
-  def after_sign_in_path_for(resource)
-    sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')
-    if request.referer == sign_in_url
-      super
-    else
-      stored_location_for(resource) || dashboard_path
-    end
-  end
-  
+
   def require_login
     unless user_signed_in?
       redirect_to root_path
     end
   end
+
+
+  # redirect to dashboard after sign in
+  private
+  def stored_location_for(resource_or_scope)
+    nil
+  end
+  def after_sign_in_path_for(resource_or_scope)
+    dashboard_path
+  end
+  
 end
