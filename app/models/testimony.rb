11def6b1d02bcac563de6dfd4c99ddc197c35546
Harrison <harrisontsai0123@gmail.com>
2013-03-27 23:06:44 -0500

    multiple photo upload, but editing still buggy

--- app/models/testimony.rb
+++ app/models/testimony.rb
@@ -1,8 +1,7 @@
 class Testimony < ActiveRecord::Base
-  attr_accessible :context, :trip_instance_id, :user_id, :photo, :photo_file_name
+  attr_accessible :context, :trip_instance_id, :user_id, :photos_attributes
   belongs_to :trip_instance
   belongs_to :user
-  has_many :photos
-  has_attached_file :photo, :styles => { :medium => "400x>", :thumb => "50x50>"}, :storage => :s3, 
-  :s3_credentials => "#{Rails.root}/config/s3.yml", :path => "/:style/:id/:filename"
+  has_many :photos, :dependent => :destroy
+  accepts_nested_attributes_for :photos, :allow_destroy => true
 end
