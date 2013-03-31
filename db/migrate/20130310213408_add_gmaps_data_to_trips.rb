84b6b56d665049e401b255b32f1345c18326ee49
callmemc <callmemc@gmail.com>
2013-03-31 02:30:02 -0700

    fixed editing testimony

--- db/migrate/20130310213408_add_gmaps_data_to_trips.rb
+++ db/migrate/20130310213408_add_gmaps_data_to_trips.rb
@@ -1,6 +1,6 @@
 class AddGmapsDataToTrips < ActiveRecord::Migration
   def change
   	add_column :trips, :latitude,  :float
-	add_column :trips, :longitude, :float
+	  add_column :trips, :longitude, :float
   end
 end
