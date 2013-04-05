1411e5e5cf642a6150e9dc8b1ca46464dbae0339
justinchan <hellojustinchan@gmail.com>
2013-04-04 20:49:24 -0700

    posts are listed by most recent

--- app/controllers/trip_instances_controller.rb
+++ app/controllers/trip_instances_controller.rb
@@ -15,6 +15,8 @@ class TripInstancesController < ApplicationController
         temp_users << tper.user
       end
     end
+    @temp_posts.sort_by!(&:updated_at)
+    @temp_posts.reverse!
     if @temp_posts.length > 3
       @temp_posts[0...3].each do |post|
           @posts << post
@@ -23,8 +25,8 @@ class TripInstancesController < ApplicationController
       @posts = @temp_posts
     end
     @users = []
-    if temp_users.length > 4
-      temp_users[0...4].each do |user|
+    if temp_users.length > 6
+      temp_users[0...6].each do |user|
         @users << user
       end
     else
