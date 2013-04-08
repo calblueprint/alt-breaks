f738e15845670f4b657ee438329d2fa842c3b1b1
callmemc <callmemc@gmail.com>
2013-04-08 16:49:50 -0700

    add break leader works. chosen still not working

--- app/controllers/trip_permissions_controller.rb
+++ app/controllers/trip_permissions_controller.rb
@@ -2,14 +2,13 @@ class TripPermissionsController < ApplicationController
 before_filter :require_login
 
 	def create
-		#@trip_permission = TripInstance.new(params[:trip_permission])
-		first_name = params[:first_name]
-		last_name = params[:last_name]
-		trip_instance_id = params[:trip_instance_id]
-		user = User.where("first_name = ? AND last_name = ?", first_name, last_name).first
-		user_id = user.id
-		@trip_permission = TripPermission.new(:user_id => user_id, :permission => 1, :trip_instance_id => trip_instance_id)
+	  user_id = params[:trip_permission][:user_id]
+	  trip_instance_id = params[:trip_permission][:trip_instance_id]
+#	  user_ids.each do |user_id|  	  
+  	TripPermission.create(:user => user = User.find(user_id), :permission => 1, :trip_instance_id => trip_instance_id)
+#  	end
 
+=begin
 		respond_to do |format|
 		  	if @trip_permission.save
 		    	format.html { redirect_to dashboard_url, notice: 'Break Leader was successfully created.' }
@@ -20,4 +19,9 @@ before_filter :require_login
 		  	end
 		end
 	end
+=end
+    respond_to do |format|
+      format.html { redirect_to dashboard_url, notice: 'Break Leader(s) successfully created.' }
+    end
+  end
 end
