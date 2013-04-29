a1a498863be262458a3bf20ed68e01f10a9aa959
Noel Moldvai <noelmoldvai@gmail.com>
2013-04-29 10:44:04 -0700

    setting s3 credentials differnetly

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -2,5 +2,5 @@ class Partner < ActiveRecord::Base
   attr_accessible :description, :name, :website, :trip_id, :logo, :partner_id
   belongs_to :trip
   has_attached_file :logo, :styles => { :medium => "400x>", :thumb => "50x50>"}, :storage => :s3, 
-  :s3_credentials => "#{Rails.root}/config/s3.yml", :path => "/:style/:id/:filename"
+  :s3_credentials => S3_CREDENTIALS, :path => "/:style/:id/:filename"
 end
