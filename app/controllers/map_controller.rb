f916d7d29c3101510634af6ad01678a454655b7f
Noah Gilmore <noah.w.gilmore@gmail.com>
2013-05-05 20:58:49 -0700

    HOLY FUCK YAYYYYYYYY ADDING TRIPS WITH LAT AND LON WORKSSS

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,7 +2,7 @@ class MapController < ApplicationController
   def index
     # the gmap function in teh view expects json so we have to render it
     # until all teh trips have dummy current_trip_instance_ids, we will only do Where the Wild Things Are
-    @json = Trip.all.to_gmaps4rails do |trip, marker|
+    @json = Trip.where("latitude IS NOT NULL").to_gmaps4rails do |trip, marker|
     	###
     	# comment in for new breakleader code:
     	# leaders = trip.break_leader_permissions.map do |permission|
