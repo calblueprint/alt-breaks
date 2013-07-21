57aaa728dee0cf615a4eaa51aad0817b40a3cd49
callmemc <callmemc@gmail.com>
2013-07-21 02:04:21 -0700

    fixed updated_at migration--if you have already migrated, will need to rake db:reset

--- db/migrate/20130327221750_remove_paperclip_from_testimony.rb
+++ db/migrate/20130327221750_remove_paperclip_from_testimony.rb
@@ -3,11 +3,11 @@ class RemovePaperclipFromTestimony < ActiveRecord::Migration
     remove_column :testimonies, :photo_file_name
     remove_column :testimonies, :photo_content_type
     remove_column :testimonies, :photo_file_size
-    remove_column :testimonies, :photo_update_at
+    remove_column :testimonies, :photo_updated_at
   end
 
   def down
-    add_column :testimonies, :photo_update_at, :datetime
+    add_column :testimonies, :photo_updated_at, :datetime
     add_column :testimonies, :photo_file_size, :integer
     add_column :testimonies, :photo_content_type, :string
     add_column :testimonies, :photo_file_name, :string
--- db/migrate/20130328035811_remove_photo_updated_from_testimony.rb
+++
@@ -1,9 +0,0 @@
-class RemovePhotoUpdatedFromTestimony < ActiveRecord::Migration
-  def up
-  	remove_column :testimonies, :photo_updated_at
-  end
-
-  def down
-  	add_column :testimonies, :photo_updated_at, :datetime
-  end
-end
