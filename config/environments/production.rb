f21ef2631edac9b29f96a32a87d6b7e9490735ec
callmemc <callmemc@gmail.com>
2013-07-22 22:20:26 -0700

    asset precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Altbreaks::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.precompile += ['gmaps4rails.base.js']
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
