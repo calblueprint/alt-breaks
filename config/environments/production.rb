afc1fda0adde7aec14f8e3d77310a98dacdc856d
callmemc <callmemc@gmail.com>
2013-07-22 22:32:03 -0700

    i dont know whats going on anymore

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Altbreaks::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.assets.precompile += ['gmaps4rails/*.js']
+  config.assets.precompile << '*.js'
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
