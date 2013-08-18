a682dce43c3c78a6c10235c24e8f8945970b0af3
callmemc <callmemc@gmail.com>
2013-08-17 22:15:05 -0700

    photo migration fix

--- db/migrate/20130817221050_photos_belong_to_trip.rb
+++ db/migrate/20130817221050_photos_belong_to_trip.rb
@@ -2,12 +2,12 @@ class PhotosBelongToTrip < ActiveRecord::Migration
   def up
     add_column :photos, :trip_instance_id, :integer
     add_column :photos, :user_id, :integer
-    remove_column :photos, :testimony_id, :integer
+    remove_column :photos, :testimony_id
   end
 
   def down
-    remove_column :photos, :trip_instance_id, :integer
-    remove_column :photos, :user_id, :integer
+    remove_column :photos, :trip_instance_id
+    remove_column :photos, :user_id
     add_column :photos, :testimony_id, :integer
   end
 end
