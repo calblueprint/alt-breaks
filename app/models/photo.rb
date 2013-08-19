ec2849e1dfdde892b90c6360345cc66803a79a69
callmemc <callmemc@gmail.com>
2013-08-19 13:09:31 -0700

    added a motherfucking sick carousel with thumbnails to the trips page

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,6 @@ class Photo < ActiveRecord::Base
   attr_accessible :caption, :testimony_id, :photo, :photo_file_name, :user_id, :trip_instance_id
   belongs_to :trip_instance
   belongs_to :user
-  has_attached_file :photo, :styles => { :medium => "400x>", :thumb => "50x50>"}, :storage => :s3,
+  has_attached_file :photo, :styles => { :medium => "400x>", :thumb => "50x50#"}, :storage => :s3,
     :s3_credentials => S3_CREDENTIALS, :path => "/testimonies/:style/:id/:filename"
 end
